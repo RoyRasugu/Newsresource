e652bf104dac8c031cbf83f26fb985614a4d9470
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-09-12 11:22:36 +0300

    add view articles function by id

--- app/views.py
+++ app/views.py
@@ -11,3 +11,11 @@ def index():
 
     message = 'Hello World'
     return render_template('index.html',message = message)
+
+@app.route('/articles/<int:source_id>')
+def articles(source_id):
+
+    '''
+    View news articles page function that returns articles details page from respective news source
+    '''
+    return render_template('articles.html', id = source_id)
