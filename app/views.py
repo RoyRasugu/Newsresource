d2f3e69c12e81756de7ccb9c0b522513a98b691c
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-09-12 16:05:49 +0300

    add title in function of index

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from datetime import time
 from flask import render_template
 from app import app
 
@@ -9,8 +10,9 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-    message = 'Hello World'
-    return render_template('index.html',message = message)
+    # message = 'Hello World'
+    title = 'Home - News Source Article Finder'
+    return render_template('index.html', title = title)
 
 @app.route('/articles/<int:source_id>')
 def articles(source_id):
